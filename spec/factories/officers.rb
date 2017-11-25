4f03a0c56b09f8e321acac9ebabcae1016ac65dd
Justin Capalbo <capalbo.justin@gmail.com>
2017-11-25 18:26:52 -0500

    Create broken bootstrap include
    Bootstrap include is not working with this commit and pages do not load.

--- spec/factories/officers.rb
+++ spec/factories/officers.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :officer do
-    
+    email        { Faker::Internet.safe_email }
+    password     "password"
+
+    # Generic single guild
+    factory :officer_with_no_members do
+      after(:create) do |officer|
+        create(:guild_with_no_members, officer: officer)
+      end
+    end
   end
+  
 end
