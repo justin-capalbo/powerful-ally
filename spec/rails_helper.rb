fafecd593e941de494a582ab56a75ba0743a6beb
Justin Capalbo <capalbo.justin@gmail.com>
2017-11-27 01:08:45 -0500

    Begin work on fixing devise login

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,8 +8,8 @@ require 'rspec/rails'
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
-require 'support/controller_macros'
-require 'support/factory_bot'
+require_relative 'support/factory_bot'
+require 'devise'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -39,9 +39,6 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
-  # Include ControllerHelpers from Devise in all :controller specs
-  config.include Devise::Test::ControllerHelpers, type: :controller
-
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
@@ -56,6 +53,9 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  # Include ControllerHelpers from Devise in all :controller specs
+  config.include Devise::Test::ControllerHelpers, :type => :controller
 
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
--- spec/support/controller_macros.rb
+++
@@ -1,9 +0,0 @@
-module ControllerMacros
-  def login_officer
-    before(:each) do
-      @request.env["devise.mapping"] = Devise.mappings[:officer]
-      officer = FactoryBot.create(:officer)
-      sign_in officer
-    end
-  end
-end
